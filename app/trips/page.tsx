f19147983c0d6a09cf908d0031c627aef1294212
Dhanush Y <dhanushyrh@gmail.com>
2024-04-05 00:32:16 +0530

    Profile

---
+++ app/trips/page.tsx
@@ -0,0 +1,23 @@
+
+export default async function Page() {
+    return (
+        <main className="font-mono flex min-h-screen flex-col p-6">
+            <div className="flex flex-row">
+                <div className="flex basis-4/12">
+                    <div className="px-4 py-6">
+                        <h2>The Demo Trip</h2>
+                        <p className="mt-4">Here at Notion we use this template to help teams plan, design and develop products with the greatest chance for success. It helps teams think through their work more deeply, improves asynchronous communication with other teams, and creates space for collaboration.</p>
+                    </div>
+                </div>
+                <div className="flex grow">
+                    <div className="mx-auto my-auto items-center">
+                        Map Area!!
+                    </div>
+                    <div className="mx-auto">
+                
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
